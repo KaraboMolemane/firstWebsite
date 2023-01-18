b7d594c8314bbce6e3f04cf676af1aefca24c3c0
KaraboMo <karabo.molemane@adaptit.com>
2023-01-18 18:37:54 +0200

    Removing whitespaces in the code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-
     
     //debugger
     if(JSON.parse(sessionStorage.getItem("quoteFood"))){
         $("#quote-food").find(".like").addClass('like-yes');
         $("#quote-food").find(".like").removeClass('like-no');
 
-    }  
+    } 
+
     if(JSON.parse(sessionStorage.getItem("quoteProg"))){
         $("#quote-prog").find(".like").addClass('like-yes');
         $("#quote-prog").find(".like").removeClass('like-no');
 
-    }  
-   
+    }     
 
     $(window).on("unload",function(){
         sessionStorage.setItem("quoteFood", $("#quote-food").find(".like").hasClass('like-yes'));
